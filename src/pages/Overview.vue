d2a13351c12b0ff98ea8b3f7a4a75fe9df7a508e
Jan <jlangela@student.tgm.ac.at>
2020-04-21 17:07:48 +0200

    Styled Reminders and Added Delete/ Edit

--- src/pages/Overview.vue
+++ src/pages/Overview.vue
@@ -1,41 +1,46 @@
 <template>
-  <q-page class="bg-grey-3 column">
-    <q-list 
-      class="bg-white"
-      separator
-      bordered>
-      <!--
-        Rendering a <label> tag (notice tag="label")
-        so QCheckboxes will respond to clicks on QItems to
-        change Toggle state.
-      -->
+  <q-page class="bg-grey-3 column block">
 
-      <q-item 
-        v-for="(reminder,index) in reminders"
-        :key="reminder.title"
-        clickable
-        @click="reminder.active = !reminder.active"
-      v-ripple>
-        <q-item-section avatar>
-          <q-checkbox 
-            
-            v-model="reminder.active"
-            :color="reminder.color" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label :class="reminder.active ? 'reminder-active' : 'reminder-inactive'">{{reminder.title}}</q-item-label>
-        </q-item-section>
-        <q-item-section
-        side>
-        <q-btn 
-        @click.stop="confirmDelete(index)"
-        round 
-        dense
-        icon="delete" 
-        :color="reminder.color"/>
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <q-card
+      v-for="(reminder,index) in reminders"
+      :key="reminder.title"
+      clickable
+      @click="reminder.active = !reminder.active"
+      :class='["reminder q-mx-sm q-mt-md"]'
+    >
+    <q-card-section
+    horizontal
+    class="row">
+      <q-card-section
+        :class='["col "+"bg-"+reminder.color, reminder.active ? "reminder-active":"reminder-inactive"]'
+      >
+        <div class="text-overline" v-if="reminder.active">Active</div>
+        <div class="text-overline" v-else>Inactive</div>
+        <div class="text-h5 q-mt-sm q-mb-xs">{{reminder.title}}</div>
+        <div class="text-caption text-white description ">
+          {{reminder.description}}
+        </div>
+      </q-card-section>
+      <q-card-section 
+        class="bg-white absolute-right q-pa-xs row">
+        <q-card-actions vertical class="justify-center reminder-options">
+          <q-btn 
+            @click.stop="confirmDelete(index)"
+            flat 
+            round 
+            color="accent" 
+            icon="edit" />
+          <q-btn 
+          class="self-center"
+            flat 
+            round 
+            color="red" 
+            icon="delete" 
+            @click.stop="confirmDelete(index)"/>
+        </q-card-actions>
+      </q-card-section>
+      </q-card-section>
+    </q-card>
     <q-btn 
       @click="add"
       
@@ -53,7 +58,7 @@ import { LocalStorage } from 'quasar'
 export default {
   data() {
     return{
-      reminders:LocalStorage.getItem("reminders")
+      reminders:JSON.parse(LocalStorage.getItem("reminders"))
     }
   },
   methods:
@@ -66,6 +71,7 @@ export default {
       persistent: true
     }).onOk(() => {
       this.reminders.splice (index, 1)
+      this.$q.localStorage.set("reminders", JSON.stringify(this.reminders));
     })
     },
     showNotif () {
@@ -75,11 +81,44 @@ export default {
       })
     },
     add(){
-      window.console.log("add")
+
+      let reminders = [
+        {
+          title:'Drink Water',
+          description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+          active:true,
+          activeon:[
+            0,1,2,3,4,5,6
+          ],
+          color:'light-blue-5',
+          time:[
+            "10:00",
+            "18:00"
+          ]
+        },
+        {
+          title:'Eat Food',
+          description:'Breakfeast',
+          active:true,
+          activeon:[
+            0,1,2,3,4,5,6
+          ],
+          color:'light-green-5',
+          time:[
+            "10:00",
+            "18:00"
+          ]
+        }
+      ]
+
+      window.console.log("add");
       
-      this.$q.sessionStorage.set("reminders", "hello")
-      let bo =this.$q.localStorage.get.item("reminders")
-      window.console.log(bo)
+      this.$q.localStorage.set("reminders", "blu");
+      this.$q.localStorage.set("reminders", JSON.stringify(reminders));
+      
+      let bo =this.$q.localStorage.get.item("reminders");
+      window.console.log(JSON.parse(bo));
+      this.reminders = JSON.parse(LocalStorage.getItem("reminders"))
     }
   }
 }
@@ -115,10 +154,19 @@ export default {
 
  
 <style lang="scss">
-.reminder-active{
-  color: black;
+.reminder{
+
 }
 .reminder-inactive{
-  color: gray;
+  filter: grayscale(0.71);
 }
+.reminder-options{
+  filter: grayscale(0) !important;
+}
+.description{
+  max-width: 80%;
+  white-space: nowrap;
+  overflow: hidden;
+}
+
 </style>
