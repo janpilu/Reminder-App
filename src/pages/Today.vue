6a700a16f52e67900b3b195f4986455c31143163
Jan <jlangela@student.tgm.ac.at>
2020-04-23 15:07:32 +0200

    Today Page now displays Tasks relevant for the day sorted by Time

--- src/pages/Today.vue
+++ src/pages/Today.vue
@@ -1,18 +1,17 @@
 <template>
-  <q-page class="flex row">
+  <q-page class="flex block">
     <div class="q-pa-md row col-12">
-      <div v-for=" r in todaysReminders" :key="r.title">
         <q-card 
-        :class='"q-ml-md bg-"+r.color'
-        >
+        v-for=" r in this.alltoday" :key="r.title + r.time"
+        :class='["card q-ml-md q-mb-md bg-"+r.color]'
+        ><!-- ,this.isPast(r.time[0]) ? "past": "past"-->
           <q-card-section>
             <div class="text-h5">{{r.title}}</div>
           </q-card-section>
           <q-card-section>
-           
+           <div class="text-subtitle2">{{r.time[0]}}</div>
           </q-card-section>
         </q-card>
-      </div>
     </div>
   </q-page>
 </template>
@@ -35,89 +34,79 @@ export default {
     getTodaysReminders(){
       this.reminders = JSON.parse(this.$q.localStorage.getItem("reminders"));
       window.console.log(this.reminders)
+      let temp = Object.assign({}, this.reminders);
     
-      for(let reminder in this.reminders){
-        if(this.reminders[reminder].activeon.includes(this.todaysDay)){
-          this.alltoday.push(this.reminders[reminder])
+      for(let reminder in temp){
+        if(temp[reminder].activeon.includes(this.todaysDay)){
+          this.alltoday.push(temp[reminder])
         }
       }
-
-      let i = 0
-      for(let reminder of this.alltoday){
-        window.console.log(reminder)
-        if(reminder.time.length>1){
-          window.console.log(reminder)
-          for(let t in reminder.time){
-            window.console.log(reminder.time.lenght)
-            this.split.push(reminder)
-            this.split[t].time = []
-            this.split[t].time.push(reminder.time[t])
+    },
+    splitReminder(){
+      let alltemp = this.alltoday.slice(0)
+      for(let i=0;i<this.alltoday.length;i++){
+        console.log(i+this.alltoday[i].title)
+        let temp=[];
+        if(this.alltoday[i].time.length>1){
+          
+          let times = this.alltoday[i].time.slice(0);//works
+          //console.log(times)
+          for(let j=0;j<times.length;j++){
+            temp.push(Object.assign({}, this.alltoday[i]))
+            temp[j].time=[times[j]]
           }
+          console.log(alltemp)
+          alltemp = alltemp.concat(temp)
         }
       }
+      let filtered = alltemp.filter(rem => rem.time.length==1)
+      this.alltoday= filtered;
+    },
+    sortReminder(){
+      this.alltoday.sort(function(b,a){
 
-      
-      /**
-      let i = 0
-      for(let reminder of alltoday){
-        if(reminder.time.length>1){
-          window.console.log(reminder)
-          window.console.log(reminder.time)
-          let j = 0;
-          let temp =[]
-          for(let timestamp of reminder.time){
-            temp.push(reminder)
-
-            window.console.log(timestamp)
-            temp[j].time = [timestamp];
-            window.console.log(temp[j].time)
-
-            j++
-          }
-          window.console.log(temp)
-        }
-        i++
-      }*/
-
-
-
-
-          /** 
-        if(alltoday[reminder].time.length>1){
-          window.console.log(alltoday[reminder].time)
-          let times = alltoday[reminder].time; 
-          let temps = [];
-          let i = 0;
-          for(let r of alltoday[reminder].time){
-            temps.push(alltoday[reminder])
-            window.console.log(temps)
-            temps[i].time=[times[i]]
-            window.console.log(r)
-            window.console.log(temps[i])
-            //temps[i].time.push(r)
-            i++
-          }
-          window.console.log(temps)
-          //alltoday.splice(reminder)
-          //alltoday.concat(times)
-        }
+        const datea = date.extractDate(a.time[0], 'HH:mm')
+        const dateb = date.extractDate(b.time[0], 'HH:mm')
+        window.console.log(datea)
+        return dateb - datea;
+      });
+    },
+    isPast(time){
+      let temp = [time, currentTime]
+      this.temp.sort(function(b,a){
+        const datea = date.extractDate(a, 'HH:mm')
+        const dateb = date.extractDate(b, 'HH:mm')
+        return dateb - datea;
+      });
+      if (temp[0]===time){
+        return true
       }
-      for(let reminder in alltoday){
-        window.console.log(alltoday)
-      }
-      this.todaysReminders=alltoday;
-      */
+      return false
     }
+
 
   },
   computed:{
     //...mapState('store', ['reminders']),
     todaysDay(){
       return date.formatDate(Date.now(), 'dddd');
+    },
+    currentTime(){
+      return date.formatDate(Date.now(), 'HH:mm');
     }
   },created(){
     console.log("hu")
     this.getTodaysReminders()
+    this.splitReminder()
+    this.sortReminder()
   }
 }
 </script>
+<style lang="scss">
+.card{
+  height: auto;
+}
+.past{
+  filter: grayscale(100%);
+}
+</style>
